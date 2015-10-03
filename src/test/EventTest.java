c67c2923636b1eb0ef0ca5b80e45353e2e077430
Yee Hui Poh <yhpoh91@gmail.com>
2015-10-03 15:12:14 +0800

    Added toString for event with test

--- src/test/EventTest.java
+++ src/test/EventTest.java
@@ -1,9 +1,10 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
 import org.junit.Test;
@@ -19,20 +20,319 @@ public class EventTest {
 //	}
 	
 	@Test
-	public void testToString() {
+	public void testGeneric() {
 		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
 		calendarStart.set(2015, 9, 20, 10, 33, 25);
 		Calendar calendarEnd = Calendar.getInstance();
 		calendarEnd.set(2015, 9, 21, 11, 34, 26);
 		
 		Event event = new Event();
-		event.setId("abc");
-		event.setTitle("def");
+		event.setId("testId");
+		event.setTitle("testTitle");
 		event.setStartDateTime(calendarStart);
 		event.setEndDateTime(calendarEnd);
 		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
 		
-		assertEquals("id: abc, title: def, startDateTime: Tue Oct 20 10:33:25 SGT 2015, endDateTime: Wed Oct 21 11:34:26 SGT 2015, priority: MEDIUM, ", event.toString());
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testWithGroups1Item() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		event.addGroup("group 1");
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [group 1], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testWithGroups2Items() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		event.addGroup("group 1");
+		event.addGroup("group 2");
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [group 1, group 2], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoId() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: null, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoTitle() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: null, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoStartDateTime() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: null, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoEndDateTime() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: null, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoPriority() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: null, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoLocation() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setNotes("test note");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: null, "
+				+ "notes: test note, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoNotes() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setReminder(calendarStart);
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: null, "
+				+ "reminder: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "groups: [], ",
+				event.toString());
+	}
+	
+	@Test
+	public void testNoReminder() {
+		Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		calendarStart.set(2015, 9, 20, 10, 33, 25);
+		Calendar calendarEnd = Calendar.getInstance();
+		calendarEnd.set(2015, 9, 21, 11, 34, 26);
+		
+		Event event = new Event();
+		event.setId("testId");
+		event.setTitle("testTitle");
+		event.setStartDateTime(calendarStart);
+		event.setEndDateTime(calendarEnd);
+		event.setPriority(Priority.MEDIUM);
+		event.setLocation("test location");
+		event.setNotes("test note");
+		
+		assertEquals("id: testId, "
+				+ "title: testTitle, "
+				+ "startDateTime: Tue Oct 20 10:33:25 SGT 2015, "
+				+ "endDateTime: Wed Oct 21 11:34:26 SGT 2015, "
+				+ "priority: MEDIUM, "
+				+ "location: test location, "
+				+ "notes: test note, "
+				+ "reminder: null, "
+				+ "groups: [], ",
+				event.toString());
 	}
 
 }
--- src/utils/Event.java
+++ src/utils/Event.java
@@ -1,7 +1,11 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+
+import sun.rmi.runtime.Log;
 
 public class Event {
 	private String id;
@@ -23,6 +27,23 @@ public class Event {
 		this.location = null;
 		this.notes = null;
 		this.reminder = null;
+		this.groups = new ArrayList<String>();
+	}
+
+	public String toString() {
+		String eventString = "";
+
+		eventString += String.format("id: %s, ", (this.id != null) ? this.id : "null");
+		eventString += String.format("title: %s, ", (this.title != null) ? this.title : "null");
+		eventString += String.format("startDateTime: %s, ", (this.startDateTime != null) ? this.startDateTime.getTime() : "null");
+		eventString += String.format("endDateTime: %s, ", (this.endDateTime != null) ? this.endDateTime.getTime() : "null");
+		eventString += String.format("priority: %s, ", (this.priority != null) ? this.priority.name() : "null");
+		eventString += String.format("location: %s, ", (this.location != null) ? this.location : "null");
+		eventString += String.format("notes: %s, ", (this.notes != null) ? this.notes : "null");
+		eventString += String.format("reminder: %s, ", (this.reminder != null) ? this.reminder.getTime() : "null");
+		eventString += String.format("groups: %s, ", Arrays.toString(this.groups.toArray()));
+
+		return eventString;
 	}
 
 	public String getId() {
@@ -92,9 +113,24 @@ public class Event {
 	public List<String> getGroups() {
 		return groups;
 	}
-
-	public void setGroups(List<String> groups) {
-		this.groups = groups;
+	
+	public void addGroup(String group){
+		this.groups.add(group);
+	}
+	
+	public void removeGroup(String group){
+		int position = -1;
+		
+		for(int i = 0; i < groups.size(); i++){
+			if(groups.get(i).equals(group)){
+				position = i;
+				break;
+			}
+		}
+		
+		if(position >= 0){
+			this.groups.remove(position);
+		}
 	}
 
 }
