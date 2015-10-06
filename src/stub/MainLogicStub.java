d83cd255734fd4ba265ad9c4da42edd0da181140
Yee Hui Poh <yhpoh91@gmail.com>
2015-10-06 10:24:26 +0800

    created simple main logic stub for gui use

---
+++ src/stub/MainLogicStub.java
@@ -0,0 +1,106 @@
+package stub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import calendrier.MainLogic;
+import utils.Command;
+import utils.Event;
+import utils.Priority;
+
+public class MainLogicStub extends MainLogic {
+	List<Event> events = new ArrayList<>();
+	Event event = null;
+	
+	@Override
+	public Command execute(String command) {
+		Command cmd = null;
+		if(command.equals("add abc")){
+			cmd = Command.ADD;
+			event = new Event();
+			event.setId("abcId");
+			event.setTitle("abc");
+			
+			
+			events.add(event);
+		}
+		else if(command.equals("update id abcId, priority low")){
+			cmd = Command.UPDATE;
+			for(Event e : events){
+				if(e.getId().equals("abcId")){
+					e.setPriority(Priority.LOW);
+					event = e;
+					break;
+				}
+			}
+		}
+		else if(command.equals("delete abcId")){
+			cmd = Command.DELETE;
+			for(Event e : events){
+				if(e.getId().equals("abcId")){
+					events.remove(e);
+					event = e;
+					break;
+				}
+			}
+		}
+		else if(command.equals("view abcId")){
+			cmd = Command.VIEW;
+			for(Event e : events){
+				if(e.getId().equals("abcId")){
+					event = e;
+					break;
+				}
+			}
+		}
+		else if(command.equals("undelete")){
+			cmd = Command.UNDELETE;
+			events = new ArrayList<>();
+			
+			event = new Event();
+			event.setId("abcId");
+			event.setTitle("abc");
+			
+			events.add(event);
+		}
+		else if(command.equals("undo")){
+			cmd = Command.UNDO;
+			
+			if(events.size() > 0){
+				for(Event e : events){
+					if(e.getId().equals("abcId")){
+						if(e.getPriority() == Priority.LOW){
+							e.setPriority(null);
+						}
+						else {
+							events.remove(e);
+						}
+						break;
+					}
+				}
+			}
+			else {
+				events = new ArrayList<>();
+				
+				event = new Event();
+				event.setId("abcId");
+				event.setTitle("abc");
+				
+				events.add(event);
+			}
+		}
+		return cmd;
+	}
+
+	@Override
+	public Event getEvent() {
+		return event;
+	}
+
+	@Override
+	public List<Event> getAllEvents() {
+		return events;
+	}
+	
+
+}
