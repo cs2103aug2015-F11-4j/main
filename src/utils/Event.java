47c14344b8b50fd835a400e65ed8b721826a437f
HMXHIU <hiumengxiong@hotmail.com>
2015-10-12 22:13:39 +0800

    Implemented fromString function for Event

--- src/utils/Event.java
+++ src/utils/Event.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 public class Event {
 	private static final String NULL = "null";
@@ -132,5 +133,142 @@ public class Event {
 			this.groups.remove(position);
 		}
 	}
+	
+	public void fromString(String dataInput){
 
+		int[] startDate= new int[6], endDate= new int[6], remindDate = new int[6];
+		String title, id, location, notes, group;
+		String[] splitedData = new String[11];
+		Priority prior;
+		
+		splitedData=dataInput.split(", ", 10);
+		id = removeName(splitedData[0]);
+		title = removeName(splitedData[1]);
+		if (splitedData[4] != null) {
+			prior = determinePrior(removeName(splitedData[4]));
+		} else {
+			prior = null;
+		}
+		location = removeName(splitedData[5]);
+		notes = removeName(splitedData[6]);
+		if (!removeName(splitedData[2]).equals("null")) {
+			startDate = convertDate(removeName(splitedData[2]));
+			Calendar calendarStart = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+			calendarStart.set(startDate[0], startDate[1], startDate[2], startDate[3], startDate[4], startDate[5]);
+			setStartDateTime(calendarStart);
+		} else {
+			startDate = null;
+		}
+		if (!removeName(splitedData[3]).equals("null")) {
+			endDate = convertDate(removeName(splitedData[3]));
+			Calendar calendarEnd = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+			calendarEnd.set(endDate[0], endDate[1], endDate[2], endDate[3], endDate[4], endDate[5]);
+			setEndDateTime(calendarEnd);
+		} else {
+			endDate = null;
+		}
+		if (!removeName(splitedData[3]).equals("null")) {
+			remindDate = convertDate(removeName(splitedData[7]));
+			Calendar calendarReminder = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+			calendarReminder.set(remindDate[0], remindDate[1], remindDate[2], remindDate[3], remindDate[4],
+					remindDate[5]);
+			setReminder(calendarReminder);
+		} else {
+			remindDate = null;
+		}
+		group = removeName(splitedData[8]);
+
+		setId(id);
+		setTitle(title);
+		setPriority(prior);
+		setLocation(location);
+		setNotes(notes);
+
+	}
+	
+	// remove the tag in string. Example: ID: abc, this method will remove ID:
+	// and return abc.
+	private String removeName(String input) {
+		String[] splitedData = new String[2];
+		splitedData = input.split(": ", 2);
+		return splitedData[1];
+	}
+
+	// convert date from string format(MM DD HH:MM:SS YY) to int array
+	private int[] convertDate(String input) {
+		String[] splitedData = new String[6];
+		int[] date = new int[6];
+		int[] time = new int[3];
+
+		splitedData = input.split(" ", 6);
+		date[0] = Integer.parseInt(splitedData[5]);
+		date[1] = convertMonth(splitedData[1]) - 1;
+		date[2] = Integer.parseInt(splitedData[2]);
+		time = convertTime(splitedData[3]);
+		date[3] = time[0];
+		date[4] = time[1];
+		date[5] = time[2];
+
+		return date;
+	}
+
+	// convert time from string format(HH:MM:SS) to int array
+	private int[] convertTime(String input) {
+		String[] splitedData = new String[3];
+		int[] time = new int[3];
+
+		splitedData = input.split(":", 3);
+		time[0] = Integer.parseInt(splitedData[0]);
+		time[1] = Integer.parseInt(splitedData[1]);
+		time[2] = Integer.parseInt(splitedData[2]);
+		return time;
+	}
+
+	// convert string month to int
+	private int convertMonth(String input) {
+		switch (input) {
+		case "Jan":
+			return 1;
+		case "Feb":
+			return 2;
+		case "Mar":
+			return 3;
+		case "Apr":
+			return 4;
+		case "May":
+			return 5;
+		case "Jun":
+			return 6;
+		case "Jul":
+			return 7;
+		case "Aug":
+			return 8;
+		case "Sep":
+			return 9;
+		case "Oct":
+			return 10;
+		case "Nov":
+			return 11;
+		case "Dec":
+			return 12;
+		}
+		return 0;
+	}
+
+	// convert string to priority data type
+	private Priority determinePrior(String input) {
+		switch (input) {
+		case "HIGH":
+			return Priority.HIGH;
+		case "MEDIUM":
+			return Priority.MEDIUM;
+		case "VERY_LOW":
+			return Priority.VERY_LOW;
+		case "VERY_HIGH":
+			return Priority.VERY_HIGH;
+		case "LOW":
+			return Priority.LOW;
+		}
+		return Priority.MEDIUM;
+	}
 }
