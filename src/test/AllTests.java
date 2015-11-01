43376f1e1119a1e035115201197a020069ba6c87
Yee Hui Poh <yhpoh91@gmail.com>
2015-11-01 21:57:37 +0800

    Added tests for IdMapper with full path coverage

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CalendrierTest.class, EventHandlerTest.class, EventGeneratorTest.class, EventTest.class,
-		ParserTest.class, StorageManagerTest.class,
+		ParserTest.class, StorageManagerTest.class, IdMapperTest.class, 
 		MainLogicFullTest.class, MainLogicClassTest.class, ReminderManagerTest.class })
 public class AllTests {
 
--- src/utils/IdMapper.java
+++ src/utils/IdMapper.java
@@ -5,57 +5,67 @@ import java.util.*;
 public class IdMapper {
 	private static IdMapper idMapper = null;
 	private HashMap<String, String> map = null;
-	
-	private IdMapper(){
+
+	private IdMapper() {
 		map = new HashMap<>();
 	}
-	
+
 	/**
 	 * Gets an instance of the mapper
-	 * @return	instance of mapper
+	 * 
+	 * @return instance of mapper
 	 */
-	public static IdMapper getInstance(){
-		if(idMapper == null){
+	public static IdMapper getInstance() {
+		if (idMapper == null) {
 			idMapper = new IdMapper();
 		}
 		return idMapper;
 	}
-	
+
 	/**
-	 * Creates Mapping for Short ID to Actual ID 
-	 * @param shortId	Short ID from the User/GUI
-	 * @param actualId	Actual ID for storage
+	 * Creates Mapping for Short ID to Actual ID
+	 * 
+	 * @param shortId
+	 *            Short ID from the User/GUI
+	 * @param actualId
+	 *            Actual ID for storage
 	 */
-	public void set(String shortId, String actualId){
-		map.put(shortId, actualId);
+	public void set(String shortId, String actualId) {
+		if (shortId != null && actualId != null) {
+			map.put(shortId, actualId);
+		}
 	}
-	
+
 	/**
 	 * Clear mapping for the short ID
-	 * @param shortId	Short ID to be cleared
+	 * 
+	 * @param shortId
+	 *            Short ID to be cleared
 	 */
-	public void clear(String shortId){
+	public void clear(String shortId) {
 		map.remove(shortId);
 	}
-	
+
 	/**
 	 * Clears all mappings
 	 */
-	public void clear(){
+	public void clear() {
 		map.clear();
 	}
-	
+
 	/**
 	 * Gets the actual ID (Storage ID)
-	 * @param shortId	Short ID from the User/GUI
-	 * @return	the actual ID for storage
+	 * 
+	 * @param shortId
+	 *            Short ID from the User/GUI
+	 * @return the actual ID for storage
 	 */
-	public String getActualId(String shortId){
+	public String getActualId(String shortId) {
 		String actualId = null;
-		if(map.containsKey(shortId)){
+		if (map.containsKey(shortId)) {
 			actualId = map.get(shortId);
 		}
-		
+
 		return actualId;
 	}
 
